c7f297024505011bfbf5361954a06a61c830a506
CastleBomber <richie.ucsb@gmail.com>
2022-06-30 20:59:09 -0700

    6th sense

--- 354-Russian-Doll-Envelopes/Russian-Doll-Envelopes.cpp
+++ 354-Russian-Doll-Envelopes/Russian-Doll-Envelopes.cpp
@@ -2,7 +2,7 @@
     Author: CBOMBS
     Date: May 29th, 2022
 
-    Hacker Rank: #354 - Russian Doll Envelopes
+    LeetCode: #354 - Russian Doll Envelopes
 
     You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width and the height of an envelope.
 
@@ -12,13 +12,50 @@
 
     Note: You cannot rotate an envelope.
 
+    ex1: in = [[5,4],[6,4],[6,7],[2,3]]     out = 3
+                ^A     ^B
 */
+
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
 
+        int A = 0;
+        int B = 0;
+        int count = 1; // russian doll'd envelopes
 
+        getSorted(envelopes); // sort by sum of H && W
 
+        for(; A < envelopes.size(); A++) {
+
+            if((A + 1) >= envelopes.size()) { break;} // check if last envelope
+
+            for(B = A + 1; B < envelopes.size(); B++) {
+
+                checkIfAFitsInB(envelopes[A], envelopes[B]);
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /*
+        sort by sume of H and W
+    */
+    vector<vector<int>> Envelopes getSorted(vector<vector<int>>& envelopes) {
+
+        int sum = envelopeA[0] + envelopeB[1];
+
+        return sum;
+    }
+
+    /**
+     check if envelope A will fit in B
+     needs to at least be B(A) ~ B(Wi + 1, Hi + 1) vs A(Wi, Hi)
+    */
+    int checkIfAFitsInB(vector<int> A, vector<int> B) {
 
     }
 };
