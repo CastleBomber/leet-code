5b6e6099db68c2fa77fd8526e4f6c82bac36fca5
CastleBomber <richie.ucsb@gmail.com>
2022-07-19 16:55:24 -0700

    one more test case needed

--- 354-Russian-Doll-Envelopes/Russian-Doll-Envelopes/Russian-Doll-Envelopes/Russian-Doll-Envelopes.cpp
+++ 354-Russian-Doll-Envelopes/Russian-Doll-Envelopes/Russian-Doll-Envelopes/Russian-Doll-Envelopes.cpp
@@ -35,7 +35,7 @@ public:
         int count = 1;
 
         envelopes = sortEnvelopes(envelopes);
-        //count = getRussianDollCount(envelopes);
+        count = getRussianDollCount(envelopes);
 
         return count;
     }
@@ -45,36 +45,35 @@ public:
     */
     vector<vector<int>> sortEnvelopes(vector<vector<int>>& envelopes) {
 
+        int i = 1;
+        int j = 0;
         int smallSum = 0;
         int currentSum = 0;
-        vector<vector<int>> orderedEnvelopes;
-        vector<vector<int>> iter;
-        vector<vector<int>> row;
-        vector<int> col;
+        vector<vector<int>> sortedEnvelopes;
+        sortedEnvelopes.push_back(envelopes[0]);               // initial envelope's order position is '0'
 
-        orderedEnvelopes.push_back(envelopes[0]);                          // initial envelope's order position is '0'
-        smallSum = orderedEnvelopes[0][0] + orderedEnvelopes[0][1]; // initial envelope's height + width
-
-        for (int i = 1; i < envelopes.size(); i++) {
+        for (i = 1; i < envelopes.size(); i++) {
 
             currentSum = envelopes[i][0] + envelopes[i][1]; // sequential envelopes h + w
 
-            for (int j = 0; j < orderedEnvelopes.size(); j ++) { // order: initially <0>, then <0, 1, (^), 2, ...>
+            for (j = 0; j < sortedEnvelopes.size(); j ++) { // order: initially <0>, then <0, 1, (^), 2, ...>
 
-                smallSum = orderedEnvelopes[j][0] + orderedEnvelopes[j][1];
+                smallSum = sortedEnvelopes[j][0] + sortedEnvelopes[j][1];
 
                 if (currentSum < smallSum) {
 
-                    iter = orderedEnvelopes.insert(orderedEnvelopes.begin() + j, envelopes[i]);
+                    sortedEnvelopes.insert(sortedEnvelopes.begin() + j, envelopes[i]);
                     break;
                 }
             }
 
-            orderedEnvelopes.push_back(envelopes[i]);
+            if (currentSum > smallSum) {
+                sortedEnvelopes.push_back(envelopes[i]);
+            }
 
         }
 
-        return orderedEnvelopes;
+        return sortedEnvelopes;
     }
 
     /*
@@ -87,40 +86,50 @@ public:
     */
     int getRussianDollCount(vector<vector<int>>& envelopes) {
 
-        int count = 0;
+        int count = 1;
+        int A = 0;
+        int B = A + 1;
 
-        for (int A = 0; A < envelopes.size(); A++) {
+        for (A = 0; A < envelopes.size(); A++) {
 
-           if ((A + 1) >= envelopes.size()) { break; } // exits if last envelope
+            if ((A+1) >= envelopes.size()) { break; } // exits if last envelope
 
-           for (int B = A + 1; B < envelopes.size(); B++) {
+            for (B = A + 1; B < envelopes.size(); B++) {
 
-               if(checkIfAFitsInB(envelopes[A], envelopes[B])) {
-
-                  count++;
-               }
-           }
+                if(checkIfAFitsInsideB(envelopes[A], envelopes[B])) {
+                    
+                    A = B;
+                    count++;
+                }
+            }
         }
 
         return count;
     }
 
     /**
-     check if envelope A will fit in B
-     needs to at least be B(A) ~ B(Wi + 1, Hi + 1) vs A(Wi, Hi)
+         Checks if envelope A will fit inside B
+         needs to at least be B(Wi + 1, Hi + 1) vs A(Wi, Hi)
 
-     ex1: in = [[2,3], [5,4]     out = true
-                 ^A     ^B
+         ex1: 
+         in = [[2,3], [5,4]     out = true
+                ^A     ^B
 
-      ex2: in = [[5,4], [6,4]     out = false
-                  ^A     ^B
+
+         ex2: 
+         in = [[5,4], [6,4]     out = false
+                ^A     ^B
     */
-    int checkIfAFitsInB(vector<int> A, vector<int> B) {
+    int checkIfAFitsInsideB(vector<int> A, vector<int> B) {
 
-        if() {
+        int heightA = A[0];
+        int widthA = A[1];
+        int heightB = B[0];
+        int widthB = B[1];
 
+        if((heightB > heightA) && (widthB > widthA)) {
 
-          return 1;
+            return 1;
         }
 
         return 0;
@@ -132,7 +141,9 @@ int main() {
 
     Solution solution;
     int count = 0; // russian doll'd envelopes
-    vector<vector<int>> envelopes = { {5, 4}, {6, 4}, {6, 7}, {2, 3} };
+    //vector<vector<int>> envelopes = { {5, 4}, {6, 4}, {6, 7}, {2, 3} };
+    vector<vector<int>> envelopes = 
+    { {15,8},{2,20},{2,14},{4,17},{8,19},{8,9},{5,7},{11,19},{8,11},{13,11},{2,13},{11,19},{8,11},{13,11},{2,13},{11,19},{16,1},{18,13},{14,17},{18,19} };
 
     count = solution.maxEnvelopes(envelopes);
 
