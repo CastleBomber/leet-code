12f9c70185267fedd062fed75b2ca214ef70746f
CastleBomber <richie.ucsb@gmail.com>
2025-11-25 13:48:24 -0800

    switching from project euler to leetcode

---
+++ 1-Two-Sum/functions.py
@@ -0,0 +1,5 @@
+def HelloWorld(number):
+    sum = number
+
+    return sum
+
---
+++ 1-Two-Sum/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+********************************************************
+    Author: CBOMBS
+    Date:   November 25th, 2025
+
+    LeetCode: #1 Hello World
+
+
+    Solution:
+
+    Notes:
+        
+*********************************************************
+'''
+
+from functions import *
+import math
+
+
+def main():
+   HelloWorld(1)
+
+main()
